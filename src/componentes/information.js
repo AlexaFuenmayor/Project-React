e65d466eac82cc2a863ab239d509d3804338f94d
AlexaFuenmayor <estherf.m18@gmail.com>
2023-01-11 10:36:32 -0500

    Project created with React

--- src/componentes/information.js
+++ src/componentes/information.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Information(){
   return(
     <div className='container-information'>
-      <img className='image-information'
-      src={'../images/photographer.jpg'} />
+      <img
+      className='image-information'
+      src={'../images/photographer.jpg'}
+      alt='photo photographer'/>
 
     </div>
   )
