6e67672e911e95c6049abd4360c763e69d2e5ed1
AlexaFuenmayor <estherf.m18@gmail.com>
2023-01-11 10:09:39 -0500

    Inicialiting proyect with React

---
+++ src/componentes/information.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Information(){
+  return(
+    <div className='container-information'>
+      <img className='image-information'
+      src={'../images/photographer.jpg'} />
+
+    </div>
+  )
+
+}
